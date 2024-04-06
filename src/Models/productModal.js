bd1e9d41ea26b15c9fc044b6ebe6483c7c855b3b
yadamoon <yared241993@gmail.com>
2024-04-06 15:10:21 +0300

    i created product schema

---
+++ src/Models/productModal.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+// Define the product schema
+const productModal = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a  name'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Please provide a  description'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please provide a price'],
+    },
+    category: {
+        type: String,
+        required: [true, 'Please provide a category'],
+    },
+    image: {
+        type: String,
+        required: [true, 'Please provide a image'],
+    },
+    // createdAt: 
+        // type: Date,
+        // default: Date.now
+    
+});
+
+// Create a Product model based on the schema
+const Product = mongoose.model('Product', productModal);
+
+module.exports = Product;
