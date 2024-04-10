aad20d53bfc33c755f553f4fb92cb25a52ff4b96
yadamoon <yared241993@gmail.com>
2024-04-10 17:30:19 +0300

    updated the userSchema and bcrypt the password

--- src/Models/userModal.js
+++ src/Models/userModal.js
@@ -1,46 +1,45 @@
-const mongoose = require('mongoose')
-const bcrpty = require('bcrypt');
-const userSchema = new mongoose.Schema(
-  {
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
     firstname: {
-      type: String,
-      required: [true, 'Please provide a first name'],
+        type: String,
+        required: [true, 'Please provide a first name'],
     },
     lastname: {
-      type: String,
-      required: [true, 'Please provide a last name'],
+        type: String,
+        required: [true, 'Please provide a last name'],
     },
- 
     email: {
-      type: String,
-      required: [true, 'Please provide an email address'],
-      unique: true,
-      lowercase: true,
-      match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address'],
+        type: String,
+        required: [true, 'Please provide an email address'],
+        unique: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address'],
     },
     password: {
-      type: String,
-      required: [true, 'Please provide a password'],
-      minlength: 6,
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 6,
     },
-    role:{
-      type:String,
-      enum: ["admin","user"],
-      default:"user"
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user"
     }
-  
-  },
-  {
+}, {
     timestamps: true,
-  }
-)
-
-userSchema.pre("save", async function (next){
-  const salt =await bcrpty.genSaltSync(10);
-  this.password = await bcrpty.hash(this.password , salt);
-
+});
+userSchema.pre("save" , async function (next){
+  const salt = await bcrypt.genSaltSync(10);
+  this.password =await bcrypt.hash(this.password , salt);
 });
 
-const UserModel = mongoose.model('User', userSchema)
+// Define a method to compare passwords
+userSchema.methods.isPasswordMatched = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
-module.exports = UserModel
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
