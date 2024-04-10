0a14120642392cb8fb0ffbf6123f385b770f758f
yadamoon <yared241993@gmail.com>
2024-04-10 17:37:27 +0300

    then i created the route using post method and and get the post method working for register and login and the other  get method for get All Users used

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { createUser } = require('../Controllers/userController');
+const { createUser , loginCtrl } = require('../Controllers/userController');
 const router = express.Router();
 
 router.post('/register', createUser);
+router.post('/login',loginCtrl);
 module.exports = router;
 
