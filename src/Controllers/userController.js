00b0051da0aaabcb9c08650a9f6b5f9180e1d20c
yadamoon <yared241993@gmail.com>
2024-04-10 17:34:20 +0300

    i created loginController function and 1st selected email and password then check the email and matched password the if correct logined else loop

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,13 +1,15 @@
 const User = require("../Models/userModal");
+const {generatToken} = require("../config/jwtToken")
+const asyncHandler = require("express-async-handler")
 
 const createUser = async (req, res) => {
     try {
         const { firstname, lastname, email, password } = req.body;
-        // Check if required fields are provided
+        
         if (!firstname || !lastname || !email || !password) {
             return res.status(400).json({ error: "Please provide all required fields: first name, last name, email, and password" });
         }
-        // Create a new user document
+
         const newUser = await User.create({ firstname, lastname, email, password });
         res.json(newUser);
     } catch (error) {
@@ -16,4 +18,29 @@ const createUser = async (req, res) => {
     }
 };
 
-module.exports = { createUser };
+const loginCtrl = asyncHandler( async (req, res) => {
+    const {email , password } = req.body;
+   const findUser= await User.findOne({email});
+   if(findUser && (await findUser.isPasswordMatched(password))){
+    res.json({
+        _id:findUser?._id,
+        firstname:findUser?.firstname,
+        lastname:findUser?.lastname,
+        email:findUser?.email,
+       token:generatToken(findUser?._id),
+    });
+}else{
+    throw new Error("Invalid Credentials")
+}
+});
+
+const getAllUsers = asyncHandler(async (req, res)=>{
+    try{
+        const getUsers = await User.find();
+        res,json(getUsers);
+    }catch(error){
+throw new Error(error)
+    }
+})
+
+module.exports = { createUser, loginCtrl , getAllUsers };
