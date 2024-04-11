91266d837072688974693143a7d151630590771c
yadamoon <yared241993@gmail.com>
2024-04-11 23:19:58 +0300

    add new router for getUserById

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createUser , loginCtrl } = require('../Controllers/userController');
+const { createUser, loginCtrl, getAllUsers, getUserById } = require('../Controllers/userController');
 const router = express.Router();
 
 router.post('/register', createUser);
-router.post('/login',loginCtrl);
+router.post('/login', loginCtrl);
+router.get('/allUsers', getAllUsers);
+router.get('/:id', getUserById)
 module.exports = router;
 
