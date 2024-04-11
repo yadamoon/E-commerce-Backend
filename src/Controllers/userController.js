a74da87ccec446c02667fa063ea5c3596d5297d4
yadamoon <yared241993@gmail.com>
2024-04-11 23:16:59 +0300

    i created method for searching by ID  get-user-by

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,11 +1,11 @@
 const User = require("../Models/userModal");
-const {generatToken} = require("../config/jwtToken")
+const { generatToken } = require("../config/jwtToken")
 const asyncHandler = require("express-async-handler")
 
 const createUser = async (req, res) => {
     try {
         const { firstname, lastname, email, password } = req.body;
-        
+
         if (!firstname || !lastname || !email || !password) {
             return res.status(400).json({ error: "Please provide all required fields: first name, last name, email, and password" });
         }
@@ -18,29 +18,42 @@ const createUser = async (req, res) => {
     }
 };
 
-const loginCtrl = asyncHandler( async (req, res) => {
-    const {email , password } = req.body;
-   const findUser= await User.findOne({email});
-   if(findUser && (await findUser.isPasswordMatched(password))){
-    res.json({
-        _id:findUser?._id,
-        firstname:findUser?.firstname,
-        lastname:findUser?.lastname,
-        email:findUser?.email,
-       token:generatToken(findUser?._id),
-    });
-}else{
-    throw new Error("Invalid Credentials")
-}
+const loginCtrl = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const findUser = await User.findOne({ email });
+    if (findUser && (await findUser.isPasswordMatched(password))) {
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            token: generatToken(findUser?._id),
+        });
+    } else {
+        throw new Error("Invalid Credentials")
+    }
 });
 
-const getAllUsers = asyncHandler(async (req, res)=>{
-    try{
+const getAllUsers = asyncHandler(async (req, res) => {
+
+    try {
         const getUsers = await User.find();
-        res,json(getUsers);
-    }catch(error){
-throw new Error(error)
+        res.json(getUsers);
+    } catch (error) {
+        throw new Error(error)
     }
 })
 
-module.exports = { createUser, loginCtrl , getAllUsers };
+const getUserById = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const getById = await User.findById(id);
+        res.json(
+            getById
+        );
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = { createUser, loginCtrl, getAllUsers, getUserById };
