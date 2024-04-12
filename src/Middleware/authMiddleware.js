4f0b710b430d0e85d594f49123d42494025b7096
yadamoon <yared241993@gmail.com>
2024-04-12 16:01:57 +0300

    updated sth

--- src/Middleware/authMiddleware.js
+++ src/Middleware/authMiddleware.js
@@ -3,42 +3,47 @@ const jwt = require("jsonwebtoken")
 const asyncHandler = require("express-async-handler")
 
 
-const authMiddleware = asyncHandler(async (req,res,next)=>{
-let token;
-if(req?.headers?.authorization?.startWith("Bearer")){
-    token =req.headers.authorization.split(" ")[1];
+const authMiddleware = asyncHandler(async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+        try {
+            if (token) {
+                const decoded = jwt.verify(token, process.env.JWT_SECRET);
+                if (decoded.id) {
+                    const user = await User.findById(decoded.id);
+                    if (user) {
+                        req.user = user;
+                        next();
+                    } else {
+                        throw new Error("User not found");
+                    }
+                } else {
+                    throw new Error("Invalid token");
+                }
+            } else {
+                throw new Error("Token not provided");
+            }
+        } catch (error) {
+            throw new Error("Not Authorized: Token expired or invalid");
+        }
+    } else {
+        throw new Error("Authorization header not provided or incorrect format");
+    }
+});
+
+
+const isAdmin = asyncHandler(async (req, res, next) => {
     try {
-        if(token){
-            const decoded = jwt.verify(token.processs.env.JWT_SECRET);
-            const user = await User.findById(decoded?.id)
-            req.user =user;
-            next();
+        const { email } = req.user;
+        const adminUser = await User.findOne({ email: email });
+        if (!adminUser || adminUser.role !== "admin") {
+            throw new Error("You are not an admin");
         }
-        
+        next();
     } catch (error) {
-        throw new Error("Not Authorized token expired , please login again")
+        throw new Error(error.message);
     }
-}
-else{
-    throw new Error("there is no token attached to header")
-}
-})
+});
 
-const isAdmin = asyncHandler(async (req,res,next)=>{
-
-    const {email}= req.user;
-    try{
-        const adminUser = await User.findOne(email)
-
-        if(adminUser.role !== "Admin"){
-            console.log("You are not Admin");
- }
-        else{
-            next();
-        }
-
-    }catch(error){
-        throw new Error(error)
-    }
-}
-)
+module.exports = {authMiddleware , isAdmin};
