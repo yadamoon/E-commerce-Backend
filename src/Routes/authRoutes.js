b999aa59278564c735b36e59c28f021effd6a83c
yadamoon <yared241993@gmail.com>
2024-04-12 07:51:20 +0300

    then i created new router for updated user using put method

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { createUser, loginCtrl, getAllUsers, getUserById } = require('../Controllers/userController');
+const { createUser, loginCtrl, getAllUsers, getUserById, deleteUserById, updateUserById } = require('../Controllers/userController');
 const router = express.Router();
 
 router.post('/register', createUser);
 router.post('/login', loginCtrl);
 router.get('/allUsers', getAllUsers);
 router.get('/:id', getUserById)
+router.delete('/:id', deleteUserById);
+router.put('/:id',updateUserById)
+
 module.exports = router;
 
