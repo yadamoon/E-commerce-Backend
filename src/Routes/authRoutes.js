9321cbc9b6aed97fc4aebd1f661470d274129606
yadamoon <yared241993@gmail.com>
2024-04-12 17:43:12 +0300

    then created two new router for block and unblock

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const { createUser, loginCtrl, getAllUsers, getUserById, deleteUserById, updateUserById  } = require('../Controllers/userController');
-const {authMiddleware , isAdmin} = require("../Middleware/authMiddleware")
+const { createUser, loginCtrl, getAllUsers, getUserById, deleteUserById, updateUserById  , blockUser , unBlockUser} = require('../Controllers/userController');
+const { authMiddleware, isAdmin } = require("../Middleware/authMiddleware")
 const router = express.Router();
-
 router.post('/register', createUser);
-router.post('/login',loginCtrl,isAdmin);
+router.post('/login', loginCtrl);
 router.get('/allUsers', getAllUsers);
-router.get('/:id', authMiddleware,isAdmin,getUserById)
+router.get('/:id', authMiddleware, isAdmin, getUserById);
 router.delete('/:id', deleteUserById);
-router.put('/:id',authMiddleware ,updateUserById)
+router.put('/:id', authMiddleware, updateUserById);
+router.put('/block-user/:id',authMiddleware , isAdmin , blockUser);
+router.put('/unblock-user/:id',authMiddleware , isAdmin , unBlockUser);
 
 module.exports = router;
 
