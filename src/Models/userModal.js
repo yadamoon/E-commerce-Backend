c08a72d383bfa4ec16cef61e080325850846e680
yadamoon <yared241993@gmail.com>
2024-04-12 17:44:15 +0300

    added new isBlocked checker

--- src/Models/userModal.js
+++ src/Models/userModal.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const { boolean } = require('joi');
 
 const userSchema = new mongoose.Schema({
     firstname: {
@@ -27,23 +28,29 @@ const userSchema = new mongoose.Schema({
         enum: ["admin", "user"],
         default: "user"
     },
-    cart:{
+
+    isBlocked: {
+        type: Boolean,
+        default: false
+    },
+
+    cart: {
         type: [String], // or specify the type of items in the array, e.g., [mongoose.Schema.Types.ObjectId]
         default: [],
     },
-    
-    address:[{type:mongoose.Schema.Types.ObjectId, ref:"Address"}],
-    product:[{type:mongoose.Schema.Types.ObjectId, ref:"Product"}],
+
+    address: [{ type: mongoose.Schema.Types.ObjectId, ref: "Address" }],
+    product: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
 }, {
     timestamps: true,
 });
-userSchema.pre("save" , async function (next){
-  const salt = await bcrypt.genSaltSync(10);
-  this.password =await bcrypt.hash(this.password , salt);
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Define a method to compare passwords
-userSchema.methods.isPasswordMatched = async function(enteredPassword) {
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
