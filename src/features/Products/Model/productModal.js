ea33025ce8fccefca67ef66984a816dfdf371b44
yadamoon <yared241993@gmail.com>
2024-04-14 13:09:33 +0300

    i created very modern products schema or model

--- src/features/Products/Model/productModal.js
+++ src/features/Products/Model/productModal.js
@@ -1,34 +1,41 @@
 const mongoose = require('mongoose');
 
-// Define the product schema
-const productModal = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please provide a  name'],
-    },
-    description: {
-        type: String,
-        required: [true, 'Please provide a  description'],
-    },
-    price: {
-        type: Number,
-        required: [true, 'Please provide a price'],
-    },
-    category: {
-        type: String,
-        required: [true, 'Please provide a category'],
-    },
-    image: {
-        type: String,
-        required: [true, 'Please provide a image'],
-        unique: true
-    },
-},
-{
-    timestamps: true,
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true , 'Please provide a  title']
+  },
+  description: {
+    type: String,
+    required: [true , 'Please provide a  description']
+  },
+  price: {
+    type: Number,
+    required: [true , 'Please provide a  price'],
+    min: 0
+  },
+  categories: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
+  }],
+  brand: {
+    type: String,
+    required: [true , 'Please provide a  brand']
+  },
+  imageUrls: [{
+    type: String,
+    required: [true , 'Please provide a  imgeUrl']
+  }],
+  stockQuantity: {
+    type: Number,
+    required: [true , 'Please provide a  StockQuantity'],
+    min: 0
+  }
+  
+},{
+    timestamps:true
 });
 
-// Create a Product model based on the schema
-const Product = mongoose.model('Product', productModal);
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
