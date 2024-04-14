f29703c5ee043caa5d9ee7319368a83ff9915889
yadamoon <yared241993@gmail.com>
2024-04-14 13:10:27 +0300

    i created catagoris schema

--- src/features/Products/Routes/productRoutes.js
+++ src/features/Products/Routes/productRoutes.js
@@ -1,5 +1,5 @@
-// const express = require('express');
-// const {createProduct} = require('../Controllers/productController');
-// const router = express.Router();
-// router.post('/register',createProduct);
-// module.exports =router;
+const express = require('express');
+const {createProduct} = require('../Controllers/productController');
+const router = express.Router();
+router.post('/register',createProduct);
+module.exports =router;
