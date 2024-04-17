3c6b0039bb55299047689faef972857ffdfe1a43
yadamoon <yared241993@gmail.com>
2024-04-17 15:08:43 +0300

    updated path

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,29 +1,9 @@
-const express = require('express');
-const { createUser,
-    loginCtrl,
-    getAllUsers,
-    getUserById,
-    deleteUserById,
-    updateUserById,
-    blockUser,
-    unBlockUser,
-    handleRefreshToken,
-    logOut
- } = require('../controllers/userController');
-const { authMiddleware, isAdmin } = require("../middleware/authMiddleware")
-const router = express.Router();
-router.post('/register', createUser);
-router.post('/login', loginCtrl);
-router.get('/allUsers', getAllUsers);
-router.get('/:id', authMiddleware, isAdmin, getUserById);
-router.get('/refresh', handleRefreshToken);
-router.get('/logOut',logOut)
-router.delete('/:id', deleteUserById);
-router.put('/:id', authMiddleware, updateUserById);
-router.put('/block-user/:id', authMiddleware, isAdmin, blockUser);
-router.put('/unblock-user/:id', authMiddleware, isAdmin, unBlockUser);
+const express = require('express')
+const AuthController = require('../controllers/authController')
 
+const router = express.Router()
+const authController = new AuthController()
 
+router.post('/login', authController.login.bind(authController))
 
-module.exports = router;
-
+module.exports = router
