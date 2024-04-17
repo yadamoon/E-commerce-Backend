db7aabec5905b19cd8e1e59b519651f87eacc6cc
yadamoon <yared241993@gmail.com>
2024-04-17 15:30:48 +0300

    deleted

--- src/config/generateRefreshToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken'); 
-
-const generatRefreshToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "3d" });
-}
-
-module.exports = { generatRefreshToken };
