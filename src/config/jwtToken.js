3e7b3d00e47200b1430611d5249611ec0f1a2b23
yadamoon <yared241993@gmail.com>
2024-04-17 15:29:57 +0300

    deleted

--- src/config/jwtToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken'); 
-
-const generatToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-}
-
-module.exports = { generatToken };
